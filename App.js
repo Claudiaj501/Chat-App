80d6cfbc01ee9187d063113fcdc483e9fe3676ea
Claudiaj501 <104376973+Claudiaj501@users.noreply.github.com>
2022-11-22 00:24:26 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import Chat from "./components/Chat";
+import Start from "./components/Start";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
